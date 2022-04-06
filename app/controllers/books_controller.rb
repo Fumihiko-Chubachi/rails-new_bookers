05b8915e03f45af694fb53d656a827e6b9e7b532
EC2 Default User <[aria.bravemen@gmail.com]>
2022-04-06 08:22:40 +0000

    投稿フォーム作成&保存機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,24 @@
 class BooksController < ApplicationController
-  def new
-  end
 
   def index
+    @new = Book.new
+    @lists = Book.all
+  end
+  
+  def create
+    new = Book.new(book_params)
+    new.save
+    redirect_to '/index'
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get '/top' => 'homes#top'
   
-  get 'books/new'
-  get 'books/index'
+  get '/index' => 'books#index'
+  post 'new' => 'books#create'
   get 'books/show'
   get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
