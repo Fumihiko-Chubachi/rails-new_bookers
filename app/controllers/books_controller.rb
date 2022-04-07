ef501c893c56fbaea9d09adb6471440ec421b675
EC2 Default User <[aria.bravemen@gmail.com]>
2022-04-07 05:33:08 +0000

    削除機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,18 +17,22 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @back = Book.all
   end
 
   def edit
     @edit = Book.find(params[:id])
-    @back = Book.all
   end
   
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to books_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to "/books"
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   get '/top' => 'homes#top'
-  
-  get '/index' => 'books#index'
-  post 'new' => 'books#create'
-  get 'show/:id' => "books#show", as: 'book'
-  get 'book/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'book/:id' => 'books#update', as: 'update_book'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
