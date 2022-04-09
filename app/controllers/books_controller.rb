7a63450a6277950794a6dd72214f63ade268357c
EC2 Default User <[aria.bravemen@gmail.com]>
2022-04-09 07:07:40 +0000

    Task2完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,13 @@ class BooksController < ApplicationController
   def index
     @new = Book.new
     @lists = Book.all
+    
   end
   
   def create
     @new = Book.new(book_params)
     if  @new.save
-      flash[:create] = 'Book was successfully created.'
+      flash[:create] = "successfully create"
       redirect_to book_path(@new.id)
     else
       @lists = Book.all
@@ -27,7 +28,7 @@ class BooksController < ApplicationController
   def update
     @edit = Book.find(params[:id])
     if @edit.update(book_params)
-      flash[:update] = 'Book was successfully updated.'
+      flash[:update] = "successfully update"
       redirect_to book_path(@edit.id)
     else
       render :edit
@@ -37,7 +38,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    flash[:destroy] = 'Book was successfully destroyed.'
+    flash[:destroy] = "successfully destroy"
     redirect_to "/books"
   end
   
